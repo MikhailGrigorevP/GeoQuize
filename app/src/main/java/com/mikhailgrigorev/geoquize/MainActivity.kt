922bb2b9c7fb1fb00997b41fb7b862d916ea6005
Mikhail <16112000m@gmail.com>
2020-05-07 13:28:35 +0300

    - GeoQuiz 1.1 -- Multiply questions -- Prev button -- Image buttons -- Question is clickable too

--- app/src/main/java/com/mikhailgrigorev/geoquize/MainActivity.kt
+++ app/src/main/java/com/mikhailgrigorev/geoquize/MainActivity.kt
@@ -1,21 +1,69 @@
 package com.mikhailgrigorev.geoquize
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val mQuestionBank = arrayOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var mCurrentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        question_text_view.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size;
+            updateQuestion();
+        }
+
         true_button.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(true);
         }
         false_button.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false);
         }
+        next_button.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size;
+            updateQuestion();
+        }
+        prev_button.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex - 1);
+            if (mCurrentIndex == -1){
+                mCurrentIndex = mQuestionBank.size - 1
+            }
+            updateQuestion();
+        }
+        updateQuestion();
     }
+
+    private fun updateQuestion() {
+        val question = mQuestionBank[mCurrentIndex].getTextResId()
+        question_text_view.setText(question)
+    }
+
+    private fun checkAnswer(userPressedTrue: Boolean) {
+        val answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue
+        var messageResId = 0
+        messageResId = if (userPressedTrue == answerIsTrue) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+
 }
